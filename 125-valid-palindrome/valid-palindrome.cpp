a7d36fc08098f31c3706120ab88c9b0ba61861c7
Meghana Govindraj <169067416+Meghanagovindhraj@users.noreply.github.com>
2026-03-06 22:36:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (53.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,15 +4,9 @@ public:
         int i=0, j=s.length()-1;
         
         while(i<j){
-            if(!isalnum(s[i])){
-            i++;continue;
-        }
-        if(!isalnum(s[j])){
-            j--;continue;
-        }
-        if(tolower(s[i])!=tolower(s[j])){
-            return false;
-        }
+            if(!isalnum(s[i])){i++;continue;}
+            if(!isalnum(s[j])){j--;continue;}
+            if(tolower(s[i])!=tolower(s[j])){return false;}
             i++;
             j--;
         }
